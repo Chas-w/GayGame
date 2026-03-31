0d5a65feeba1750791e998a3fe6c360eb0e7a1e4
vicstroie <158312626+vicstroie@users.noreply.github.com>
2026-03-31 14:27:56 -0400

    3d hinge joint test

---
+++ Scenes/hand3d.gd
@@ -0,0 +1,21 @@
+extends RigidBody3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _input(event: InputEvent) -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(delta: float) -> void:
+	if Input.is_key_pressed(KEY_D):
+		apply_force(Vector3.RIGHT * 50)
+	if Input.is_key_pressed(KEY_A):
+		apply_force(Vector3.LEFT * 50)
+	if Input.is_key_pressed(KEY_W):
+		apply_force(Vector3.UP * 50)
