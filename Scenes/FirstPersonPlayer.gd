536b8a5545ad2a5effdf0247c6f9cdff07ce66a8
vicstroie <158312626+vicstroie@users.noreply.github.com>
2026-04-04 14:28:22 -0400

    made a first person controller

---
+++ Scenes/FirstPersonPlayer.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+#Attributes
+@export_range(0, 5) var speed : float
+@export_range(0, 0.015) var sensitivity : float
+
+@onready var pivot := $CameraPivot
+@onready var camera := $CameraPivot/Camera3D
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event is InputEventMouseMotion:
+		pivot.rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(60))
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "up", "down")
+	var direction = (pivot.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+
+	move_and_slide()
