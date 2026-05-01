74559b3fb55d7a2941e885b318d54c3c47e40db5
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-05-01 17:42:56 -0400

    Point and click movement

---
+++ Prototyping/CHASE/Movement Test/ClickToMove.gd
@@ -0,0 +1,32 @@
+extends CharacterBody3D
+
+@export var nav_agent : NavigationAgent3D
+@export var cam : Camera3D
+@export var move_speed : float
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(nav_agent.is_navigation_finished()):
+		return
+	else:
+		_move_to_target(delta,move_speed)
+
+func _move_to_target(delta,speed):
+	var target_position = nav_agent.get_next_path_position()
+	var direction = global_position.direction_to(target_position)
+	
+	velocity = direction * speed
+	move_and_slide()
+
+
+func _input(event):
+	if(Input.is_action_just_pressed("click")):
+		var mouse_pos = get_viewport().get_mouse_position()
+		var ray_length = 1000
+		var from = cam.project_ray_origin(mouse_pos)
+		var to = from + cam.project_ray_normal(mouse_pos) * ray_length
+		var space = get_world_3d().direct_space_state
+		var ray_query = PhysicsRayQueryParameters3D.new()
+		ray_query.from = from
+		ray_query.to = to
+		var result = space.intersect_ray(ray_query)		
+		nav_agent.set_target_position(result.position)
