debeddd431b8fd0c6fd92269e672f7d80200da4c
vicstroie <158312626+vicstroie@users.noreply.github.com>
2026-05-22 16:23:19 -0400

    Pong prototype made

---
+++ Prototyping/VICTOR/Scenes/pong_manager.gd
@@ -0,0 +1,65 @@
+extends Node3D
+
+#Serialize Variables
+@export_group("Attributes")
+@export var pong_ball : RigidBody3D
+@export var default_throw_force : float
+@export var default_up_force : float
+
+@export_group("References")
+@export var ball_start : Node3D
+@export var canvas : Control
+@export var table : Node3D
+
+
+
+#Local Variables
+var ready_to_throw : bool
+var start_point : Vector2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#Start throwing the ball
+	if Input.is_action_pressed("click") and !ready_to_throw:
+		ready_to_throw = true
+		start_point = canvas.get_global_mouse_position()
+	
+	#Rest for debugging
+	if Input.is_action_pressed("rightclick"):
+		pong_ball.freeze = true
+		pong_ball.position = ball_start.position
+		pong_ball.rotation = ball_start.rotation
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_released("click") and ready_to_throw:
+		#Set Variabless
+		pong_ball.freeze = false
+		ready_to_throw = false
+		
+		#Get Mouse Delta
+		var mouse_delta_vector = canvas.get_global_mouse_position() - start_point
+		var force_vector = Vector2(mouse_delta_vector.x / canvas.get_viewport_rect().size.x, mouse_delta_vector.y / canvas.get_viewport_rect().size.y)
+		print(force_vector)
+		print(force_vector.length())
+		
+		#Get Force Amount
+		var throw_force = default_throw_force * force_vector.length()
+		
+		#Get Force Direction
+		force_vector = force_vector.normalized()
+		print(force_vector.normalized())
+		print(table.basis.x)
+		print(table.basis.z)
+		#var throw_direction : Vector3 = (table.basis.x * force_vector.x) + (table.basis.z * force_vector.y)
+		var throw_direction = Vector3(force_vector.x, 0, force_vector.y)
+		print(throw_direction)
+
+		#Apply Impulse
+		pong_ball.apply_impulse(table.basis.y * default_up_force)
+		pong_ball.apply_impulse(throw_direction * throw_force)
+		
