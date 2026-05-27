b8f1d7af7beee35873894275d6f3ee327904f9a1
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2026-05-26 23:04:16 -0400

    gonna have to do dialogue by myself, because plugins SUCK

--- Prototyping/CHASE/Movement Test/scripts/dialogue_controller.gd
+++ Prototyping/CHASE/Movement Test/scripts/dialogue_controller.gd
@@ -1,1 +1,4 @@
 extends Node3D
+
+func _start_dialogue(dialogue : Resource):
+	DialogueManager.show_dialogue_balloon(dialogue, "start")
--- Prototyping/CHASE/Movement Test/scripts/interaction_manager.gd
+++ Prototyping/CHASE/Movement Test/scripts/interaction_manager.gd
@@ -2,4 +2,3 @@ extends Node3D
 
 @export_category("Setup")
 @export var has_dialogue : bool
-@export var dialogue_controller : Node3D
